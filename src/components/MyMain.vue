1aa12d900b163172fbbe08c9efceefe373ae0990
stefanodisanza <87936828+stefanodisanza@users.noreply.github.com>
2023-03-09 13:08:22 +0100

    FEAT add cover

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -3,56 +3,84 @@
         <!-- film -->
         <div class="container-card" v-for="film in listFilm" :key="film.id">
             <div class="card">
-                <div> Titolo: {{ film.title }}</div>
-                <div>Titolo Originale: {{ film.original_title }}</div>
-                <div>Lingua: <img :src="insertFlag(film.original_language)"></div>
-                <div>Voto: {{ film.vote_average }}</div>
+                <div class="cover">
+                    <img v-if="film.poster_path !== null" :src="coverUrl + film.poster_path" :alt="film.title" />
+                    <img v-else class="cover-netflix"
+                        src="https://i.pinimg.com/564x/01/e1/35/01e135a5bcabe81ce279076de8dfbfd9.jpg" alt="">
+                    <!-- container info -->
+                    <div class="container-info">
+                        <div>Titolo: {{ film.title }}</div>
+                        <div>Titolo Originale: {{ film.original_title }}</div>
+                        <div class="language">Lingua: <img class="mini_flag" :src="insertFlag(film.original_language)" />
+                        </div>
+                        <div>Voto: {{ film.vote_average }}</div>
+                    </div>
+                </div>
             </div>
         </div>
         <!-- serie tv -->
+        <!-- poster_path -->
         <div class="container-card" v-for="tv in listTv" :key="tv.id">
             <div class="card">
-                <div> Titolo: {{ tv.name }}</div>
-                <div>Titolo Originale: {{ tv.original_name }}</div>
-                <div>Lingua: <img :src="insertFlag(tv.original_language)"></div>
-                <div>Voto: {{ tv.vote_average }}</div>
+                <div class="cover">
+                    <img v-if="tv.poster_path !== null" :src="coverUrl + tv.poster_path" :alt="tv.title" />
+                    <img v-else class="cover-netflix"
+                        src="https://i.pinimg.com/564x/01/e1/35/01e135a5bcabe81ce279076de8dfbfd9.jpg" alt="">
+                    <!-- container info -->
+                    <div class="container-info">
+                        <div>Titolo: {{ tv.name }}</div>
+                        <div>Titolo Originale: {{ tv.original_name }}</div>
+                        <div class="language">Lingua: <img class="mini_flag" :src="insertFlag(tv.original_language)" />
+                        </div>
+                        <div>Voto: {{ tv.vote_average }} <span v-for="star in starsVote(tv.vote_average)" :key="star"><img
+                                    :src="star" :alt="star"></span></div>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
 </template>
 <script>
-const itFlag = '/it_flag.png'
-const enFlag = '/en_flag.png'
-const frFlag = '/fr_flag.png'
-
+const itFlag = '/it_flag.png';
+const enFlag = '/en_flag.png';
+const frFlag = '/fr_flag.png';
 
 export default {
-    name: 'MyMain',
+    name: "MyMain",
     props: {
         listFilm: Array,
         listTv: Array,
     },
     data() {
         return {
+            coverUrl: "http://image.tmdb.org/t/p/w342",
             itFlag: itFlag,
             enFlag: enFlag,
             frFlag: frFlag,
-        }
+            arrayStar: []
+        };
     },
     methods: {
         insertFlag(item) {
             if (item === 'it') {
-                return this.itFlag
+                return this.itFlag;
             } else if (item === 'en') {
-                return this.enFlag
+                return this.enFlag;
             } else if (item === 'fr') {
-                return this.frFlag
+                return this.frFlag;
             } else {
-                return 'https://www.unr.edu/main/images/news/blog/progress-flag.jpg'
+                return 'https://www.unr.edu/main/images/news/blog/progress-flag.jpg';
             }
         },
-    },
-}
+        starsVote(vote) {
+            const arrayStar = [];
+            for (let i = 0; i < Math.floor(vote / 2); i++) {
+                arrayStar.push("https://www.pngall.com/wp-content/uploads/9/Golden-Star-PNG-Image-File.png");
+            }
+            return arrayStar;
+        }
+    }
+};
 </script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -67,7 +95,7 @@ main {
 .container-card {
     padding: 20px;
     // debug
-    height: 300px;
+    height: 100%px;
     width: 20%;
     background-color: #2c3e50;
     border: 1px solid black;
@@ -79,11 +107,15 @@ main {
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
     height: 100%;
 }
 
 img {
+    width: 100%;
+}
+
+.mini_flag {
     width: 15px;
 }
 </style>
